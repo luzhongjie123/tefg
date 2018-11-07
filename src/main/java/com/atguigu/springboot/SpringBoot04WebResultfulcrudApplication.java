d941c1b55244ba4943ad47c12b8326fe0618cf9b
zhangsan <123>
2018-11-07 17:57:39 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springboot/SpringBoot04WebResultfulcrudApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebResultfulcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot04WebResultfulcrudApplication.class, args);
+	}
+}
